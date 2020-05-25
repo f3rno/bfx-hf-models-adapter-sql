eeade85516c4223497484b62c8300576d96d09dd
Cris Mihalache <me@f3rno.com>
2020-05-25 18:11:29 +0700

    (wip)

--- index.js
+++ index.js
@@ -1,79 +1,52 @@
 'use strict'
 
-// Automatically parses decimals as floats
-// NOTE: This results in a theoretical loss of precision
-// TODO: Use BigNum
-// https://github.com/tgriesser/knex/issues/927#issuecomment-291066092
-const pg = require('pg')
-const PG_DECIMAL_OID = 1700
-const PG_BIGINT_OID = 20
+const HFDBSQLDBAdapter = require('./lib/index')
 
-pg.types.setTypeParser(PG_DECIMAL_OID, parseFloat)
-pg.types.setTypeParser(PG_BIGINT_OID, 'text', parseInt)
+/**
+ * This is an adapter around {@npm knex} for the Bitfinex Honey Framework. To
+ * use, initialize an instance of the `bfx-hf-models` database with it as an
+ * adapter.
+ *
+ * ### Features
+ * * Compatible with multiple flavours of SQL via {@npm knex}
+ * * High-scalability vs
+ *   {@link module:bfx-hf-models-adapter-lowdb|bfx-hf-models-adapter-lowdb}
+ * * Fully compatible with {@link module:bfx-hf-models|bfx-hf-models} & the HF
+ *   system ({@link module:bfx-hf-data-server|bfx-hf-data-server},
+ *   {@link module:bfx-hf-algo-server|bfx-hf-algo-server},
+ *   {@link module:bfx-hf-algo|bfx-hf-algo}, etc)
+ *
+ * ### Installation
+ *
+ * ```bash
+ * npm i --save bfx-hf-models-adapter-sql
+ * ```
+ *
+ * ### Quickstart & Example
+ *
+ * ```js
+ * const HFDBSQLAdapter = require('bfx-hf-models-adapter-sql')
+ * const { schema: HFDBBitfinexSchema } = require('bfx-hf-ext-plugin-bitfinex')
+ * const HFDB = require('bfx-hf-models')
+ *
+ * const PSQL_CONNECTION = '...'
+ *
+ * const db = new HFDB({
+ *   schema: HFDBBitfinexSchema,
+ *   adapter: HFDBSQLAdapter({
+ *     connection: PSQL_CONNECTION,
+ *     clientType: 'pg'
+ *   }),
+ * })
+ *
+ * const { Candle } = db
+ * const candles = await Candle.getAll()
+ *
+ * console.log(`read ${candles.length} candles`)
+ * ```
+ *
+ * @license Apache-2.0
+ * @module bfx-hf-models-adapter-sql
+ */
 
-const debug = require('debug')('bfx:hf:models:adapter-sql')
-const _isEmpty = require('lodash/isEmpty')
-const _isString = require('lodash/isString')
-const _includes = require('lodash/includes')
-const knex = require('knex')
-
-const mapMethods = require('./lib/map_methods')
-const genericMethods = require('./lib/generic_methods')
-const collectionMethods = require('./lib/collection_methods')
-
-const CLIENT_TYPES = ['pg', 'mysql', 'sqlite']
-
-module.exports = ({
-  clientType,
-  connection
-}) => {
-  if (!_includes(CLIENT_TYPES, clientType)) {
-    throw new Error([
-      `unsupported client type: ${clientType}`,
-      `must be one of ${CLIENT_TYPES.join(',')}`
-    ].join(' '))
-  }
-
-  if (_isEmpty(connection)) {
-    throw new Error('no connection params provided')
-  }
-
-  // NOTE: Don't log connection string for security
-  debug('[%s] connecting...', clientType)
-
-  const db = knex({
-    client: clientType,
-    acquireConnectionTimeout: 10 * 1000,
-    connection,
-    pool: {
-      min: 2,
-      max: 25
-    }
-  })
-
-  const dbInit = (model) => {
-    const { path, name } = model
-
-    if (!_isString(path) || _isEmpty(path)) {
-      throw new Error(`model DB path not string or empty for model ${name}`)
-    }
-
-    const knexOnPath = db(path)
-
-    knexOnPath._knex = db
-
-    return knexOnPath
-  }
-
-  const close = db.destroy.bind(db)
-
-  return {
-    db,
-    dbInit,
-    close,
-    mapMethods,
-    genericMethods,
-    collectionMethods,
-    name: 'SQL'
-  }
-}
+module.exports = HFDBSQLDBAdapter
